0a7665e6494f6d15888e5dbbe18d80178b242e1a
changhwan0411 <fani411@naver.com>
2026-04-09 17:33:42 +0900

    20260409

--- market/views/main_view.py
+++ market/views/main_view.py
@@ -5,3 +5,7 @@ bp = Blueprint('main', __name__, url_prefix='/')
 @bp.route('/')
 def index():
     return render_template('main.html')
+
+@bp.route('/detail')
+def detail():
+    return render_template('detail.html', product=None)
