3b8649bf835914562d433379a7600093e8b8d23f
changhwan0411 <fani411@naver.com>
2026-04-13 17:31:22 +0900

    20260413

--- market/__init__.py
+++ market/__init__.py
@@ -4,26 +4,31 @@ from flask_sqlalchemy import SQLAlchemy
 
 import os
 
-db = SQLAlchemy()
-migrate = Migrate()
+db = SQLAlchemy()    # ORM 도구
+migrate = Migrate()  # 테이블 구조 변경 시 DB에 반영해주는 도구
 
 def create_app():
     app = Flask(__name__)
 
     BASE_DIR = os.path.dirname(__file__)
-
-    db_path = os.path.abspath(os.path.join(BASE_DIR, '..', 'market.db'))
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(db_path)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'market.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     migrate.init_app(app, db)
 
+    SECRET_KEY = 'dev'
+    app.config['SECRET_KEY'] = SECRET_KEY
+
     from . import models
 
     # 블루프린트
-    from .views import main_view, auth_views, product_view # view 파일들 임포트
+    from .views import main_view, auth_views , product_view, favorite_view, deal_view, review_view  # view 파일들 임포트
     app.register_blueprint(main_view.bp)
     app.register_blueprint(auth_views.bp) # 회원가입/로그인 블루프린트
+    # app.register_blueprint(favorite_view.bp)
+    # app.register_blueprint(deal_view.bp)
+    # app.register_blueprint(review_view.bp)
 
     return app
+
--- market/models.py
+++ market/models.py
@@ -1,47 +1,37 @@
 from market import db
-from sqlalchemy.orm import backref
 
-# table 구조
+# 유저
 class User(db.Model):
-    id = db.Column(db.String(20), primary_key=True)      # user id (PK) - db 내부 구분용
-    username = db.Column(db.String(50), unique=True)     # username id
-    password = db.Column(db.String(20), unique=True)     # pw
-    phone = db.Column(db.String(15), unique=True)        # phone number
-    email = db.Column(db.String(50), unique=True)        # email
-
-class Admin(db.Model):
-    id = db.Column(db.String(20), primary_key=True)      # admin id (PK)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete = 'CASCADE', onupdate = 'CASCADE'))
-    user = db.relationship('User')
-
-class Item(db.Model):
-    id = db.Column(db.String(20), primary_key=True)
-    item_name = db.Column(db.String(50), unique=True)
-    item_description = db.Column(db.String(500), unique=True)
-    item_price = db.Column(db.String(50), unique=True)
-    status_id = db.Column(db.Integer, db.ForeignKey('item.id', ondelete = 'CASCADE', onupdate = 'CASCADE'))
-    status = db.relationship('Item_status')
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id', ondelete = 'CASCADE', onupdate = 'CASCADE'))
-    datetime = db.Column(db.DateTime)
-
-class Item_status(db.Model):
-    id = db.Column(db.String(20), primary_key=True)
-    status = db.Column(db.String(20), unique=True)
-    item_id = db.Column(db.Integer, db.ForeignKey('item.id', ondelete = 'CASCADE', onupdate = 'CASCADE'))
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id', ondelete = 'CASCADE', onupdate = 'CASCADE'))
-
-class Category(db.Model):
-    id = db.Column(db.String(20), primary_key=True)
-    category_name = db.Column(db.String(50), unique=True)
-    item_id = db.Column(db.Integer, db.ForeignKey('item.id', ondelete = 'CASCADE', onupdate = 'CASCADE'))
-
-class Review(db.Model):
-    id = db.Column(db.String(20), primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete = 'CASCADE', onupdate = 'CASCADE'))
-    review_content = db.Column(db.String(500), unique=True)
-    datetime = db.Column(db.DateTime)
-
-class Deal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    item_id = db.Column(db.Integer, db.ForeignKey('item.id', ondelete = 'CASCADE', onupdate = 'CASCADE'))
-    datetime = db.Column(db.DateTime)
+    login_id = db.Column(db.String(50), unique=True)  # 로그인용
+    username = db.Column(db.String(50))               # 닉네임
+    password = db.Column(db.String(200))
+    email = db.Column(db.String(50), unique=True)
+    phone = db.Column(db.String(20), unique=True)
+
+# 관리자
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    login_id = db.Column(db.String(50), unique=True) # 관리자 로그인
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(200))
+
+# 상품
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_title = db.Column(db.String(100), unique=False) # 상품 제목
+    item_price = db.Column(db.String(100)) # 가격
+    item_reg_datetime = db.Column(db.DateTime(), unique = False) # 상품 업로드 시간
+    item_description = db.Column(db.String(500)) # 상품 내용
+
+# 상품 상태
+class Item_Status(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_status = db.Column(db.String(100), unique=False)  # 상품 상태
+
+# 카테고리
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(100), unique=False)
+
+
